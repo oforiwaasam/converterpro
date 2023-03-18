98f3631e7221500574497daefed4fbf23e828b18
Lily Sam <41793292+oforiwaasam@users.noreply.github.com>
2023-03-18 00:25:17 -0400

    added more conversion code and more tests

--- converterpro/weight_converter.py
+++ converterpro/weight_converter.py
@@ -1,10 +1,35 @@
-class Kilogram():
+THOUSAND = 1e3
+MILLION = 1e6
+BILLION = 1e9
+TRILLION = 1e12
+KILOGRAMS_TO_POUNDS = 2.2046226218488
 
-    def __init__(self, value) -> None:
+class Gram():
+    def __init__(self, value):
         self.value = value
 
-    def to_gram(self):
-        return self.value * 1000
-    
-    def to_kilogram(self):
+    def convert_to_grams(self):
         return self.value
+    
+    def convert_to_milligrams(self):
+        return self.value * THOUSAND
+
+    def convert_to_kilograms(self):
+        return self.value / THOUSAND
+
+class Kilogram():
+
+    def __init__(self, value):
+        self.value = value
+
+    def convert_to_grams(self):
+        return self.value * THOUSAND
+    
+    def convert_to_milligrams(self):
+        return self.value * MILLION
+    
+    def convert_to_kilograms(self):
+        return self.value
+    
+    def convert_to_pounds(self):
+        return self.value * KILOGRAMS_TO_POUNDS
